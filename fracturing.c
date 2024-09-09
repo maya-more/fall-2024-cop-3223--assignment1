bdd42bd12b2c7c5b800a6987370c021b11c9e470
maya-more <mayactonn@gmail.com>
2024-09-09 03:46:40 +0000

    calculate width and calculate height functions

--- fracturing.c
+++ fracturing.c
@@ -3,6 +3,7 @@
 
 //gcc fracturing.c -lm && ./a.out
 
+
 double calculateDistance() {
 
     //initialize variable with user input
@@ -24,8 +25,8 @@ double calculateDistance() {
     scanf("%lf", &y2);
 
     //print point 1 and 2
-    printf("Point #1: x1 = %lf; y1= %lf\n", x1, y1);
-    printf("Point #2: x2 = %lf; y2= %lf\n", x2, y2);
+    printf("Point #1 entered: x1 = %lf; y1= %lf\n", x1, y1);
+    printf("Point #2 entered: x2 = %lf; y2= %lf\n", x2, y2);
 
     //caculating distance and initializing 
     double x_value = x2 - x1;
@@ -35,9 +36,87 @@ double calculateDistance() {
     double sub_values = pow_x_value + pow_y_value;
     double distance = sqrt(sub_values);
     printf("%lf \n", distance);
+
+    return distance;
 }
 
+/*
+//perimeter function
+double calculatePerimeter() {
+    double perimeter = 0;
+    perimeter = calculateDistance();
+    printf("\nThe perimeter of the city encompassed by your request is %lf", perimeter);
+    return 3.0;
+}
+
+*/
+
+double calculateWidth() {
+    //initialize variable with user input
+    double x1;
+    double x2;
+    double y1;
+    double y2;
+
+    printf("\nEnter point x1: ");
+    scanf("%lf", &x1);
+
+    printf("Enter point y1: ");
+    scanf("%lf", &y1);
+
+    printf("Enter point x2: ");
+    scanf("%lf", &x2);
+
+    printf("Enter point y2: ");
+    scanf("%lf", &y2);
+
+    //print point 1 and 2
+    printf("Point #1 entered: x1 = %lf; y1= %lf\n", x1, y1);
+    printf("Point #2 entered: x2 = %lf; y2= %lf\n", x2, y2);
+
+    //calculate width
+    double width = x2 - x1;
+
+    printf("\nThe width of the city encompassed by your request is %lf", width);
+}
+
+double calculateHeight() {
+    //initialize variable with user input
+    double x1;
+    double x2;
+    double y1;
+    double y2;
+
+    printf("\nEnter point x1: ");
+    scanf("%lf", &x1);
+
+    printf("Enter point y1: ");
+    scanf("%lf", &y1);
+
+    printf("Enter point x2: ");
+    scanf("%lf", &x2);
+
+    printf("Enter point y2: ");
+    scanf("%lf", &y2);
+
+    //print point 1 and 2
+    printf("Point #1 entered: x1 = %lf; y1= %lf\n", x1, y1);
+    printf("Point #2 entered: x2 = %lf; y2= %lf\n", x2, y2);
+
+    //calculate width
+    double height = y2 - y1;
+
+    printf("\nThe height of the city encompassed by your request is %lf", height);
+}
+
+
+
 int main(int argc, char** argv) {
+    calculateHeight();
+    calculateWidth();
     calculateDistance();
+    //calculatePerimeter();
     return 0;
 }
+
+
